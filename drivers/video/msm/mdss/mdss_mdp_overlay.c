579255ab5271b098c1eaad6f6949b64e25b424cc
Junxiao Chang <w20081@motorola.com>
2013-06-28 09:46:20 +0800

    msm:mdss: move panel mutex unlock to avoid dead lock
    ESD added panel mutex lock. In some failure case, it might have
    dead lock issue.
    
    Change-Id: Id79962cd350dd00876edf87fe4b7ed2d89add06d
    Signed-off-by: Junxiao Chang <w20081@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/548891
    SLT-Approved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Check Patch <CHEKPACH@motorola.com>
    Reviewed-by: Klocwork kwcheck <klocwork-kwcheck@sourceforge.mot.com>
    Reviewed-by: Jiang-Li Yuan <jlyuan@motorola.com>
    Reviewed-by: Dong Zhao <kevin.d.zhao@motorola.com>
    Reviewed-by: Bang Nguyen <bnguyen1@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Reviewed-by: Stephen Rossbach <ccpm12@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -2920,10 +2920,13 @@ static int mdss_mdp_overlay_on(struct msm_fb_data_type *mfd)
 			return rc;
 	}
 
+	mdss_mdp_video_unlock_panel(mdp5_data->ctl);
+
 	if (IS_ERR_VALUE(rc)) {
 		pr_err("Failed to turn on fb%d\n", mfd->index);
 		mdss_mdp_overlay_off(mfd);
 	}
+
 	return rc;
 }
 
